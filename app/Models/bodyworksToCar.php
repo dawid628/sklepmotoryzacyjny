43167c129257cee8d3dd213905703db93edf266c
Dawid <dawid628@onet.eu>
2021-07-12 15:33:39 +0200

    Done models

---
+++ app/Models/bodyworksToCar.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class bodyworksToCar extends Model
+{
+    use HasFactory;
+    protected $table = 'bodyworks_to_car';
+}
---
+++ app/Models/enginesToPart.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class enginesToPart extends Model
+{
+    use HasFactory;
+    protected $table = 'engines_to_part';
+}
